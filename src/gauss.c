1b87ebb0b6090e5747816688ac1935cb1bc973ca
witw2 <wit.winiarski@gmail.com>
2022-12-14 17:01:07 +0100

    element diag, poprawa czytelnosci, pivoting, komplementarnosc z gotowymi funkcjami

--- src/gauss.c
+++ src/gauss.c
@@ -2,17 +2,13 @@
 #include <math.h>
 
 int eliminate(Matrix *mat, Matrix *b){
-
 	int il = mat->r;
-
 	for(int n = 0; n < il - 1; n++){
 		int nmax = n;
-
 		for(int r = n+1; r < il; r++)
 			if(fabs(mat-> dane[r][n]) > fabs(mat-> dane[nmax][n]) ){
 				nmax = r;
 			}
-
 		if( nmax != n){
 			double btmp = b-> dane[n][0];
 			double *tmp = mat-> dane[n];
@@ -21,15 +17,11 @@ int eliminate(Matrix *mat, Matrix *b){
 			b-> dane[nmax][0] = btmp;
 			mat-> dane[nmax] = tmp;
 		}
-		
 		for(int r = n+1; r < il; r++){ 
-
 			if(mat-> dane[n][n]==0){
 				return 1;
 			}
-
-			double m = mat-> dane[r][n] / mat-> dane[n][n];	
-											
+			double m = mat-> dane[r][n] / mat-> dane[n][n];									
 			for(int i = n; i < il; i++ ){
 				mat-> dane[r][i] -= m * mat-> dane[n][i];
 			}
--- src/main.c
+++ src/main.c
@@ -9,23 +9,23 @@ int main(int argc, char ** argv) {
 	Matrix * A = readFromFile(argv[1]);
 	Matrix * b = readFromFile(argv[2]);
 	Matrix * matx;
-
-	if (A == NULL) return -1;
-	if (b == NULL) return -2;
+	if (A == NULL){
+		return -1;	
+	}
+	if (b == NULL){
+		return -2;
+	}
 	printToScreen(A);
 	printToScreen(b);
-
 	res = eliminate(A,b);
-	matx = createMatrix(b->r, 1);
-	if (matx != NULL) {
+	matx = createMatrix(b->r,1);
+	if (matx != NULL){
 		res = backsubst(matx,A,b);
-
 		printToScreen(matx);
 	  freeMatrix(matx);
 	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+		fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
 	}
-
 	freeMatrix(A);
 	freeMatrix(b);
 	if(res!=0)
