80cc97d0e0af65bae8ff055a7ffe1a86fbfdce6a
cyvn7 <42326412+cyvn7@users.noreply.github.com>
2022-12-14 16:49:54 +0100

    implementacja metody podstawienia wstecznego

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,22 +1,21 @@
 #include "backsubst.h"
-/**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+int  backsubst(Matrix * matx, Matrix *mat, Matrix *b) {
+	int n=mat->r;
+	if(matx->c!= 1 || b->c!=1){
+		return 2;
+	}
+	for(int i=n-1;i>=0;i--){
+		double s=0;
+		for(int j=i+1;j<n;j++){
+			s += mat->dane[i][j] * matx->dane[j][0];
+		}
+		if(mat->dane[i][i]==0 ){
+			return 1;
+		}
+		matx->dane[i][0] = (b->dane[i][0] - s) / mat->dane[i][i];
+	}
+	return 0;
 }
 
 
